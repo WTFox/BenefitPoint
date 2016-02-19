ba7a3d5187732db1e17529b406ce15e8fa366459
WTFox <Anthonyfox1988@Gmail.com>
2016-02-18 18:46:24 -0600

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='benefitpoint',
+    version='0.1',
+    packages=['benefitpoint'],
+    url='',
+    license='',
+    author='anthonyfox',
+    author_email='anthonyfox1988@gmail.com',
+    description='A python wrapper for BenefitPoint API'
+)
